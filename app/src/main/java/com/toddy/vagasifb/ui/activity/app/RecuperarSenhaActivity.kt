c7d872fd49f8e72abac248362970b810d71a10ae
biduzilla <rlyowlly19@gmail.com>
2023-03-15 14:23:10 -0300

    recuperar senha layout

--- app/src/main/java/com/toddy/vagasifb/ui/activity/app/RecuperarSenhaActivity.kt
+++ app/src/main/java/com/toddy/vagasifb/ui/activity/app/RecuperarSenhaActivity.kt
@@ -2,10 +2,16 @@ package com.toddy.vagasifb.ui.activity.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.toddy.vagasifb.databinding.ActivityRecuperarSenhaBinding
 
 class RecuperarSenhaActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityRecuperarSenhaBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recuperar_senha)
+        setContentView(binding.root)
     }
 }
